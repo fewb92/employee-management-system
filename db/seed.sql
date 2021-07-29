2db31d7b54f39dedb3ff06bab53080442383e019
fewb92 <sotojosh96@gmail.com>
2021-07-29 01:19:24 -0400

    installed dependencies, started on seed and renamed employee schema

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,25 @@
-USE employeeDB;
+USE employer_DB;
+
+INSERT INTO department (name)
+VALUES
+("Operations"),
+("Campaigns"),
+("Marketing");
+("Executive");
+
+-- INSERT INTO role (title, salary, department_id)
+-- VALUES
+-- ("Operations Manager", 150000, 1),
+-- ("Software Engineer", 120000, 1),
+-- ("Analyst", 100000, 2),
+-- ("Accountant", 200000, 2),
+-- ("Manager", 100000, 3),
+-- ("Assistant", 70000, 3);
+
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id)
+-- VALUES
+-- ("Irma", "Rivera", 1, 2),
+-- ("Luis", "Rivera", 2, null),
+-- ("Gabe", "Rivera", 3, null),
+-- ("Daniel", "McDonald", 4, null),
+-- ("Casandra", "McDonald", 5, null);
