f8fa5d13b24ea5c6e594bbe42851b76184b95107
fewb92 <sotojosh96@gmail.com>
2021-07-29 01:38:02 -0400

    updated server.js to initialize inquirer

--- server.js
+++ server.js
@@ -14,3 +14,73 @@ connection.connect((err) => {
     if (err) throw err; 
     init();
 });
+
+const init = () => {
+    inquirer.prompt({
+        name: 'option',
+        type: 'rawlist',
+        message: 'Please select from one of the options below:',
+        choices: [
+            'Add a department, role, or an employee',
+            'View a department, role, or an employee',
+            'Update an employee role',
+            'Delete a department, role or an employee',
+            'Exit the Application!'
+        ],
+    })
+    .then (response => {
+        switch(response.option) {
+            case 'Add a department, role, or an employee':
+                addEntity();
+                break;
+            case 'View a department, role, or an employee':
+                viewEntity();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+            case 'Delete a department, role or an employee':
+                deleteEntity();
+                break;
+            case 'Exit the Application!':
+                connection.end();
+                break;
+            default: 
+            console.log(`Invalid option: ${response.option}`);
+            break;
+        }
+    });
+};
+
+const viewEntity = () => {
+    inquirer.prompt({
+        name: 'option',
+        type: 'rawlist',
+        message: 'Enter an entity to view:',
+        choices: [
+            'Departments',
+            'Employee Roles',
+            'Employees',
+            'Return to previous prompts!'
+        ],
+    })
+    .then(response => {
+        switch (response.option) {
+            case 'Departments':
+                viewDepartments();
+                break;
+            case 'Employee Roles':
+                viewRoles();
+                break;
+            case 'Employees':
+                viewEmployees();
+                break;
+            case 'Return to previous prompts!':
+                init();
+                break;
+            default: 
+            console.log(`Invalid optiion ${response.option}`);
+            break;
+        }
+    });
+};
