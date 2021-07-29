119f5873f2b0cf4bfcd0d38d6af57328d24287b3
fewb92 <sotojosh96@gmail.com>
2021-07-29 03:54:16 -0400

    final additions to server.js to finish CRUD operations

--- server.js
+++ server.js
@@ -159,3 +159,187 @@ const addDepartment = () => {
             });
     });
 };
+
+const addRole = () => {
+    inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'Enter a title for this role:'
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'Enter the salary for this role:'
+        },
+        {
+            name: 'departmentID',
+            type: 'input',
+            message: 'Enter the department_id for this role:'
+        }
+    ])
+    .then(response => {
+        const query = 'INSERT INTO role SET ?';
+        connection.query(query, 
+            {
+                title: response.title,
+                salary: response.salary,
+                department_id: response.departmentID
+            },
+            (err, res) => {
+                if (err) throw err;
+                console.log(`Successfully role title ${response.title} has been added!`);
+                viewEntity();
+            });
+    });
+};
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            name: 'firstName',
+            type: 'input',
+            message: 'Enter the firstName of an employee:'
+        },
+        {
+            name: 'lastName',
+            type: 'input',
+            message: 'Enter the lastName of an employee:'
+        },
+        {
+            name: 'roleID',
+            type: 'input',
+            message: 'Enter the role_id for this employee:'
+        },
+        {
+            name: 'managerID',
+            type: 'input',
+            message: 'Enter the manager_id for this employee, if there is one:'
+        }
+    ])
+    .then(response => {
+        if(response.managerID === "") {
+            response.managerID = null
+        } 
+       
+        console.log('This is the response:', response);
+        const query = 'INSERT INTO employee SET ?';
+        connection.query(query, 
+            {
+                first_Name: response.firstName,
+                last_Name: response.lastName,
+                role_id: response.roleID,
+                manager_id: response.managerID
+            },
+            (err, res) => {
+                if (err) throw err;
+                console.log(`${response.firstName} ${response.lastName} successfully added as a new employee!`);
+                viewEntity();
+            });
+    });
+};
+
+
+const updateEmployeeRole = () => {
+    inquirer.prompt([
+        {
+            name: 'employeeID',
+            type: 'input',
+            message: 'Enter the employee_id of employee:'
+        },
+        {
+            name: 'roleID',
+            type: 'input',
+            message: 'Enter the new role_id of the employee:'
+        },
+    ])
+    .then(response => {
+        const query = `UPDATE employee SET employee.role_id = ${response.roleID} WHERE employee.id = ${response.employeeID}`;
+        connection.query(query, (err, res) => {
+            if (err) throw err;
+            console.log('Employee role_id has been updated!');
+            viewEntity();
+        });
+    });
+};
+
+const deleteEntity = () => {
+    inquirer.prompt({
+        name: 'option',
+        type: 'rawlist',
+        message: 'Enter an entity to delete:',
+        choices: [
+            'Department',
+            'Employee Role',
+            'Employee',
+            'Return to previous prompts!'
+        ],
+    })
+    .then(response => {
+        switch (response.option) {
+            case 'Department':
+                deleteDepartment();
+                break;
+            case 'Employee Role':
+                deleteRole();
+                break;
+            case 'Employee':
+                deleteEmployee();
+                break;
+            case 'Return to previous prompts!':
+                init();
+                break;
+            default: 
+            console.log(`Invalid optiion ${response.option}`);
+            break;
+        }
+    });
+};
+
+const deleteDepartment = () => {
+    inquirer.prompt({
+        name: 'departmentName',
+        type: 'input',
+        message: 'Enter the department name you would like to delete:'
+    })
+    .then(response => {
+        const query = `DELETE FROM department WHERE name = '${response.departmentName}'`;
+        connection.query(query, (err, res) => {
+            if (err) throw err;
+            console.log(`Successfully department ${response.departmentName} has been deleted!`);
+            viewEntity();
+        });
+    });
+};
+
+const deleteRole = () => {
+    inquirer.prompt({
+        name: 'title',
+        type: 'input',
+        message: 'Enter the role-title you would like to delete:'
+    })
+    .then(response => {
+        const query = `DELETE FROM role WHERE title = '${response.title}'`;
+        connection.query(query, (err, res) => {
+            if (err) throw err;
+            console.log(`Successfully role ${response.title} has been deleted!`);
+            viewEntity();
+        });
+    });
+};
+
+const deleteEmployee = () => {
+    inquirer.prompt({
+        name: 'employeeID',
+        type: 'input',
+        message: 'Enter the employee_id of the employee you would like to delete:'
+    })
+    .then(response => {
+        const query = `DELETE FROM employee WHERE id = '${response.employeeID}'`;
+        connection.query(query, (err, res) => {
+            if (err) throw err;
+            console.log(`Successfully employee_id ${response.employeeID} has been deleted!`);
+            viewEntity();
+        });
+    });
+};
